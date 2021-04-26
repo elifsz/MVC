13bb1c54d3a021fa2237b499d984764f504bde1b
elif şahingöz <elif_sahingoz@hotmail.com>
2021-04-26 05:28:25 +0300

    first step

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [StringLength(50)]
+        public String CategoryName { get; set; }
+        [StringLength(200)]
+        public String CategoryDescription { get; set; }
+        //sistemden silmek yerine çünkü hiyerarşik düzende 
+        //content de gider aktiflik durumu değiştirilip,erişim kaldırılıyor
+        public bool CategoryStatus { get; set; }
+
+        public ICollection<Heading> Headings { get; set; }
+    }
+}
