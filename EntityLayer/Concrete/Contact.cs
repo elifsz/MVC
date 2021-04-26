7b5a271c7a17ad147edd8361ba6dc44f1d40956c
elif şahingöz <elif_sahingoz@hotmail.com>
2021-04-26 04:55:30 +0300

    entity

---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Contact
+    {
+        [Key]
+        public int ContactID { get; set; }
+        [StringLength(50)]
+        public String UserName { get; set; }
+        [StringLength(50)]
+        public String UserMail { get; set; }
+        [StringLength(50)]
+        public String Subject { get; set; }
+        public String Message { get; set; }
+
+    }
+}
